db907148ed3dfeba49e761113fb349bc64173fe3
Vivek Kandathil <32603391+vivekandathil@users.noreply.github.com>
2020-08-18 18:17:07 -0400

    Added buttons to automatically evaluate on WolframAlpha, Symbolab

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 // Used to render the latex code provided in the text field
 import 'katex/dist/katex.min.css';
 import { InlineMath, BlockMath } from 'react-katex';
-import { Button, ButtonGroup, TextField, Paper } from '@material-ui/core';
+import { Button, ButtonGroup, TextField, Link, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 // For the drag and drop functionality 
 import { DropzoneArea } from 'material-ui-dropzone';
@@ -39,7 +39,7 @@ function App() {
 
   const classes = useStyles();
   // The latex code to be rendered
-  const [latex, setLatex] = React.useState('\\text{Type your } \\LaTeX \\text{ code below or drag/drop an image below. Your equation will show up here! }');
+  const [latex, setLatex] = React.useState('\\text{Type your } \\LaTeX \\text{ code or drag/drop an image below! }');
   // The files from the dropzone
   const [files, setFiles] = React.useState([]);
   const [image, setImage] = React.useState("");
@@ -62,10 +62,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1 style={{ fontFamily: 'Roboto' }}>Live Equation Editor (vivekandathil)</h1>
+      <div style={{
+        display: 'flex', alignItems: 'center', justifyContent: 'center'
+      }}>
+        <div style={{
+          borderRadius: 8,
+          height: 75,
+          margin: 16,
+          justifyContent: 'center'
+        }}>
+          <h1 style={{ fontFamily: 'Roboto', textAlign: 'center', color: 'black' }}>Live Equation Editor</h1>
+        </div>
+      </div>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: 40 }} >
 
-        <Paper style={{ height: 100, width: '60%', padding: 10 }} elevation={3} >
+        <Paper style={{ height: 140, width: '60%', padding: 10 }} elevation={3} >
           <BlockMath math={(latex === "") ? `\\mathbb{CLEAR}` : String.raw`${latex}`}
             renderError={(error) => {
               return (
@@ -89,30 +100,43 @@ function App() {
           setLatex(event.target.value);
         }}
         className={classes.latexInput}
-        style={{ marginTop: 50 }}
+        style={{ marginTop: 25, width: '60%' }}
       />
       <ButtonGroup size="large" orientation="vertical" aria-label="large outlined primary button group" style={{ marginTop: 50 }}>
+
+      </ButtonGroup>
+      <div style={{ marginBottom: 50 }}>
+        <a className={classes.symbolabButton} target='_blank' href={"https://www.symbolab.com/solver/step-by-step/" + encodeURIComponent(latex)}>
+          Symbolab
+      </a>
+        <a className={classes.wolframButton} target='_blank' href={"https://www.wolframalpha.com/input/?i=" + encodeURIComponent(latex)}>
+          Wolfram Alpha
+      </a>
         <Button className={classes.button}>Save as Image</Button>
         <Button className={classes.button} onClick={() => setLatex("")}>CLEAR</Button>
-      </ButtonGroup>
-      <DropzoneArea
-        filesLimit={1}
-        dropzoneText={"Drag an equation image here to be scanned"}
-        onChange={(fileArray) => {
-          // Upload the snippet to an S3 bucket with a randomized filename
-          const S3Client = new S3(config);
-          const fileName = `snippet_${makeid(8)}`;
 
-          // When resolved, change the imageURL to trigger the OCR API call
-          S3Client
-            .uploadFile(fileArray[0], fileName)
-            .then(data => setImageURL(data.location))
-            .catch(err => console.error(err));
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <DropzoneArea
+          dropzoneClass={classes.dropzone}
+          filesLimit={1}
+          dropzoneText={"Drag an equation image here to be scanned"}
+          onChange={(fileArray) => {
+            // Upload the snippet to an S3 bucket with a randomized filename
+            const S3Client = new S3(config);
+            const fileName = `snippet_${makeid(8)}`;
 
-          // Set the current file to the image
-          setFiles(fileArray);
-        }}
-      />
+            // When resolved, change the imageURL to trigger the OCR API call
+            S3Client
+              .uploadFile(fileArray[0], fileName)
+              .then(data => setImageURL(data.location))
+              .catch(err => console.error(err));
+
+            // Set the current file to the image
+            setFiles(fileArray);
+          }}
+        />
+      </div>
       <Wave fill="url(#gradient)">
         <defs>
           <linearGradient id="gradient" gradientTransform="rotate(90)">
@@ -126,19 +150,47 @@ function App() {
 }
 
 const useStyles = makeStyles({
+  symbolabButton: {
+    background: 'linear-gradient(45deg, #FF416C 30%, #FF4B2B 90%)',
+    border: 0,
+    borderRadius: 8,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 28,
+    padding: 20,
+
+    fontWeight: 'bold',
+    textDecoration: 'none'
+
+  },
+  wolframButton: {
+    background: 'linear-gradient(45deg, #f12711 10%, #f5af19 90%)',
+    border: 0,
+    borderRadius: 8,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 28,
+    padding: 20,
+    marginLeft: 30,
+    fontWeight: 'bold',
+    textDecoration: 'none'
+
+  },
+  dropzone: {
+    width: '60%'
+  },
   button: {
     background: 'linear-gradient(45deg, #ec38bc 30%, #7303c0 90%)',
     border: 0,
-    borderRadius: 3,
+    borderRadius: 8,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     color: 'white',
-    height: 48,
-    padding: '0 30px',
-    marginBottom: 10
+    height: 56,
+    padding: 20,
+    marginLeft: 30
   },
   latexInput: {
-    height: 210,
-    width: '48%',
+    height: 100,
     margin: 30,
   },
 });
